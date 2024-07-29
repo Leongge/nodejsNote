b316f0354ad6093ef0e6b7786e007a8f2b2a6388
leongge <leong10722@gmail.com>
2024-07-29 11:46:59 +0800

    install yargs and detect command

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
-process.argv[0] = 'node executable'
-process.argv[1] = 'javascript file'
-const command = process.argv[2];
+// process.argv[0] = 'node executable'
+// process.argv[1] = 'javascript file'
+// const command = process.argv[2];
 
-if(command === 'add'){
-    console.log('Adding note!');
-}
-else if(command === 'remove'){
-    console.log('Removing note!');
-}
+// if(command === 'add'){
+//     console.log('Adding note!');
+// }
+// else if(command === 'remove'){
+//     console.log('Removing note!');
+// }
+
+const yargs = require('yargs');
+
+yargs.version('1.1.0');
+
+yargs.command({
+    command: 'add',
+    describe: '',
+    handler: function(){
+        console.log('This add calulation here!');
+    }
+});
+
+console.log(yargs.argv);
